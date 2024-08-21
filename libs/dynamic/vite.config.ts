7c73022f55e31267d9ee661e9f269d8d07359a1a
Anders Johnson <Anders.Johnson@target.com>
2024-08-21 07:06:31 -0500

    fix: test dep preserves modules and node externals

--- libs/dynamic/vite.config.ts
+++ libs/dynamic/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
+import { nodeExternals } from 'rollup-plugin-node-externals';
 
 // https://dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma
 // https://vitejs.dev/config/
@@ -18,7 +19,12 @@ export default defineConfig({
       output: {
         assetFileNames: 'assets/[name][extname]',
         entryFileNames: '[name].js',
+        preserveModules: true,
+        preserveModulesRoot: 'src'
       },
+      plugins: [
+        nodeExternals(),
+      ]
     },
   },
 });
